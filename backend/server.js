600ac66813923e86fa887fbfbe771099121353fb
Sinchana N <sinchana2505@gmail.com>
2025-08-16 20:31:26 +0530

    Added database

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(
 
 app.use(express.json());
 
-connectDB;
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
